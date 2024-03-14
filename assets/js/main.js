449cc5a1b3655b2a11532ff56454f1dacc6945a6
binhan212 <binhanvio@gmail.com>
2024-03-14 16:47:18 +0700

    cm2

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+var chartDom = document.getElementById('main-2-chart');
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Hồ Sơ',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: '#fff',
+        borderWidth: 2
+      },
+      label: {
+        show: false,
+        position: 'outside'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '12',
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: [
+        { value: 60229, name: 'Hồ sơ đã trả kết quả trước hạn', itemStyle: { color: '#5470C6' } },
+        { value: 74694, name: 'Hồ sơ đã trả kết quả đúng hạn', itemStyle: { color: '#91CC75' } },
+        { value: 35, name: 'Hồ sơ đã trả kết quả trễ hạn', itemStyle: { color: '#FAC858' } },
+        { value: 4640, name: 'Hồ sơ đang xử lý còn hạn', itemStyle: { color: '#EE6666' } },
+        { value: 1021, name: 'Hồ sơ đang xử lý quá hạn', itemStyle: { color: '#73C0DE' } }
+      ]
+    }
+  ]
+};
+myChart.setOption(option);
